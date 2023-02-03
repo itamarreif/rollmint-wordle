558b86a47b5e5e42a143e056aff5e18d31e96461
itamar <itamar.reif@gmail.com>
2023-02-03 18:09:15 -0500

    transaction posting works

--- scripts/wordle/start.sh
+++ scripts/wordle/start.sh
@@ -8,6 +8,6 @@ echo $DA_BLOCK_HEIGHT
 wordled start \
     --rollmint.aggregator true \
     --rollmint.da_layer celestia \
-    --rollmint.da_config='{"base_url":"http://localhost:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' \
+    --rollmint.da_config='{"base_url":"http://celestia-lc:26659","timeout":60000000000,"fee":6000,"gas_limit":6000000}' \
     --rollmint.namespace_id $NAMESPACE_ID \
     --rollmint.da_start_height $DA_BLOCK_HEIGHT
