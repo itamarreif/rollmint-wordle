3ff3f0701ba5b31995ee5f5e9cd87c0450aa808e
itamar <itamar.reif@gmail.com>
2023-02-03 17:42:24 -0500

    connection refused

--- scripts/wordle/genesis.sh
+++ scripts/wordle/genesis.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wordled keys add $KEY_NAME --keyring-backend $KEYRING_BACKEND
-wordled add-genesis-account $KEY_NAME $TOKEN_AMOUNT --keyring-backend $KEYRING_BACKEND
-wordled gentx $KEY_NAME $STAKING_AMOUNT --chain-id $CHAIN_ID --keyring-backend $KEYRING_BACKEND
+(echo $KEY_PASSWD; echo $KEY_PASSWD) | wordled keys add $KEY_NAME --keyring-backend $KEYRING_BACKEND
+echo $KEY_PASSWD | wordled add-genesis-account $KEY_NAME $TOKEN_AMOUNT --keyring-backend $KEYRING_BACKEND
+echo $KEY_PASSWD | wordled gentx $KEY_NAME $STAKING_AMOUNT --chain-id $CHAIN_ID --keyring-backend $KEYRING_BACKEND
 wordled collect-gentxs
